aab58faa1c688133a3b2f64a0e829881367b7a18
Gautam Kumar Singh <gautam.singh@konicaminolta.com>
2021-05-20 14:16:59 +0800

    Basic Structure

---
+++ Algorithm/Algorithm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Algorithm
+//
+//  Created by Gautam Kumar Singh on 20/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
